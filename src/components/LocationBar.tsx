cfce027fe573bdd599e2892d89fc32a6e1b83708
fantasy995 <1642063542@qq.com>
2022-10-30 10:50:34 +0800

    位置导航组件

--- src/components/LocationBar.tsx
+++ src/components/LocationBar.tsx
@@ -1,22 +1,58 @@
-import { Breadcrumb } from 'antd';
-import React, { useEffect } from 'react';
-import { useMatch } from 'react-router-dom'
+import { Breadcrumb, Divider } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useMatch, Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
-export default function LocationBar() {
-    useEffect(() => {
+export default function LocationBar(props: {
+    title?: string
+}) {
+    const { t, i18n } = useTranslation()
+    const loction = useLocation()
+    const breadcrumbItems = [(
+        <Breadcrumb.Item key={'/'}>
+            <Link to={'/'}>{t('Home')}</Link>
+        </Breadcrumb.Item>
+    )]
+    const pathSnippets = location.pathname.split('/').filter(i => i);
+    let topCategory = null
+    let subCategory = null
+    if (pathSnippets[0] === 'detail') {
+        topCategory = pathSnippets[1]
+        subCategory = pathSnippets[2]
+    } else {
+        if (pathSnippets.length === 1) { // 一级类别首页
+            topCategory = pathSnippets[1]
+        } else if (pathSnippets.length === 2) { // 二级类别  
+            subCategory = pathSnippets[2]
+        }
+    }
+    if (topCategory) {
+        breadcrumbItems.push(
+            (
+                <Breadcrumb.Item key={`/${pathSnippets[1]}`}>
+                    <Link to={`/${pathSnippets[1]}`}>{t(`video.topCategory.${topCategory}`)}</Link>
+                </Breadcrumb.Item>
+            )
+        )
+        if (subCategory) {
+            breadcrumbItems.push(
+                (
+                    <Breadcrumb.Item key={`/${pathSnippets[1]}/${pathSnippets[2]}`}>
+                        <Link to={`/${pathSnippets[1]}/${pathSnippets[2]}`}>{t(`video.subCategory.${topCategory}.${subCategory}`)}</Link>
+                    </Breadcrumb.Item>
+                )
+            )
+        }
+    }
 
-    }, [])
     return (
-        <Breadcrumb>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>
-                <a href="">Application Center</a>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-                <a href="">Application List</a>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>An Application</Breadcrumb.Item>
-        </Breadcrumb>
+        <div>
+            <Breadcrumb>
+                {breadcrumbItems}
+            </Breadcrumb>
+            <Divider></Divider>
+        </div>
+
     )
 
 }
