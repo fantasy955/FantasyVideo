5a0fd111cbc6820c5a6380b4a9ef2843998b1e7b
fantasy995 <1642063542@qq.com>
2022-11-13 11:40:54 +0800

    修改播放器资源ip地址

--- app/api/player.js
+++ app/api/player.js
@@ -64,7 +64,7 @@ router.get('/vod/:id', (ctx) => {
                 var video = document.getElementById('video');
                 if (Hls.isSupported()) {
                     var hls = new Hls();
-                    hls.loadSource('http://0.0.0.0:5000/vod/demo/index.m3u8');
+                    hls.loadSource('http://43.139.126.249:5000/vod/demo/index.m3u8');
                     hls.attachMedia(video);
                     hls.on(Hls.Events.MANIFEST_PARSED, function () {
                         video.play();
