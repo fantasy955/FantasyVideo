0de99223d630c8d9ef3408f212824473f72808ae
fantasy995 <1642063542@qq.com>
2022-11-14 21:24:10 +0800

    抹去隐私信息

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   environment: 'dev',
   database: {
-    dbName: 'fantasyvideo',
-    host: '43.139.126.249',
+    dbName: '******',
+    host: '***********',
     port: 3306,
-    user: 'root',
-    password: '9494itsyou'
+    user: '****',
+    password: '***********'
   },
   security: {
     secretKey: "fd03f48b8aafdc949e0ed679e2fffe8db6ffe2a54ba3b3ce4eaa2a612c4a9d90ffa460b162fa30dea4a775303bd0b817",
