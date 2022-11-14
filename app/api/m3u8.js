29c06136e713a88aa789fb1b199ece9f7b9b0e11
fantasy995 <1642063542@qq.com>
2022-11-14 16:48:50 +0800

    feature: 指定链接下载m3u8视频，并存储为mp4

---
+++ app/api/m3u8.js
@@ -0,0 +1,29 @@
+const Router = require('koa-router')
+const m3u8 = require('../controller/m3u8')
+
+const router = new Router({
+    prefix: '/m3u8'
+})
+
+router.get('/download', async (ctx) => {
+    const target = ctx.query.path
+    const { title, filename } = ctx.query
+    if (target.endsWith('.m3u8')) {
+        await m3u8.downloadMedia({
+            url: target,
+            output: `${process.cwd()}/public/storage/m3u8/${title}`,
+            filename: filename,
+        })
+        ctx.body = {
+            code: 1,
+            msg: '下载成功',
+        }
+    } else {
+        ctx.body = {
+            code: 0,
+            msg: '目标路径错误'
+        }
+    }
+})
+
+module.exports = router
---
+++ app/controller/m3u8.js
@@ -0,0 +1,97 @@
+const ffmpeg = require("fluent-ffmpeg");
+const log = require('single-line-log').stdout;
+const fs = require("fs");
+const { resolve } = require("path");
+const path = require("path");
+
+/**
+ * A class to convert M3U8 to MP4
+ * @class
+ */
+class m3u8ToMp4Converter {
+    /**
+     * Sets the input file
+     * @param {String} filename M3U8 file path. You can use remote URL
+     * @returns {Function}
+     */
+    setInputFile(filename) {
+        if (!filename) throw new Error("您必须指定M3U8文件地址");
+        this.M3U8_FILE = filename;
+
+        return this;
+    }
+
+    /**
+     * Sets the output file
+     * @param {String} filename Output file path. Has to be local :)
+     * @returns {Function}
+     */
+    setOutputFile(filename) {
+        if (!filename) throw new Error("您必须指定文件路径和名称");
+        this.OUTPUT_FILE = filename;
+
+        return this;
+    }
+
+    /**
+     * Starts the process
+     */
+    start() {
+        return new Promise((resolve, reject) => {
+            if (!this.M3U8_FILE || !this.OUTPUT_FILE) {
+                reject(new Error("您必须指定输入和输出文件"));
+                return;
+            }
+
+            console.log('=========================');
+
+            ffmpeg(this.M3U8_FILE)
+                .on("error", error => {
+                    reject(new Error(error));
+                })
+                .on('progress', function (progress) {
+                    log('下载进度: 已完成 ' + (progress.percent).toFixed(2) + '%。');
+                })
+                .on("end", () => {
+                    log('下载进度: 已完成 100%。\n');
+                    console.log('=========================');
+                    resolve();
+                })
+                .outputOptions("-c copy")
+                .outputOptions("-bsf:a aac_adtstoasc")
+                .output(this.OUTPUT_FILE)
+                .run();
+        });
+    }
+}
+
+const converter = new m3u8ToMp4Converter();
+
+async function downloadMedia(opt) {
+    // 测试视频，如果链接失效的话就自己找一个
+    let url = opt.url || "https://www.hkg.haokan333.com/201903/07/qM3F7ntN/800kb/hls/index.m3u8";
+    let output = opt.output || 'video';
+    let filename = opt.filename + '.mp4' || 'video.mp4';
+
+    if (!fs.existsSync(output)) {
+        fs.mkdirSync(output, {
+            recursive: true,
+        });
+    }
+
+    await new Promise(async (resolve, reject) => {
+        try {
+            console.log("准备下载...");
+            await converter
+                .setInputFile(url)
+                .setOutputFile(path.join(output, filename))
+                .start();
+
+            resolve('ok')
+        } catch (error) {
+            resolve(new Error("哎呀，出错啦! 检查一下参数传对了没喔。", error));
+        }
+    })
+}
+
+module.exports = { downloadMedia };
